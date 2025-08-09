ac78e66c1aa407ceab95dc4d9b023de715fc30b7
kotragowda.chikkagowdru <ckotragowda@gmail.com>
2025-08-10 03:47:34 +0530

    conflict resolved

--- frontend/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home(){
-  return (<div className="p-6 max-w-6xl mx-auto">
-    <h1 className="text-2xl font-bold">AlgoDatta</h1>
-    <p className="text-gray-600">Welcome. Use the nav above.</p>
-  </div>);
-}
