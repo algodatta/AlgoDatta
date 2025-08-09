837abf93350c4e589fb9c70b55017b68dc3dcda3
kotragowda.chikkagowdru <ckotragowda@gmail.com>
2025-08-09 20:27:06 +0530

    patch2026

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,6 @@
+export default function Home(){
+  return (<div className="p-6 max-w-6xl mx-auto">
+    <h1 className="text-2xl font-bold">AlgoDatta</h1>
+    <p className="text-gray-600">Welcome. Use the nav above.</p>
+  </div>);
+}
